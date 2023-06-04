6a88d20fac2c172982f9f35dd03c85311c8d4ebd
Arkadiusz Chmura <arekchmura@gmail.com>
2023-06-04 22:06:12 +0200

    Small tweaks

--- shell/nvim/after/plugin/treesitter.lua
+++ shell/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require "nvim-treesitter.configs".setup {
+require("nvim-treesitter.configs").setup {
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
     sync_install = false,
     auto_install = true,
