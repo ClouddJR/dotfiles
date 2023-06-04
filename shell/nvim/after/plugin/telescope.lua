1372010bf9228293dea4ed26b99bb19ec7406730
Arkadiusz Chmura <arekchmura@gmail.com>
2023-06-04 19:48:26 +0200

    Add a mapping for searching hidden files

--- shell/nvim/after/plugin/telescope.lua
+++ shell/nvim/after/plugin/telescope.lua
@@ -1,5 +1,8 @@
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>pa", function()
+    builtin.find_files({ hidden = true })
+end)
 vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
