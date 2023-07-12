fc8b5aa1f416d0e26e82b5b34ae52894024422ce
Arkadiusz Chmura <arekchmura@gmail.com>
2023-07-12 22:10:43 +0200

    Add incremental selection

--- shell/nvim/after/plugin/treesitter.lua
+++ shell/nvim/after/plugin/treesitter.lua
@@ -5,6 +5,13 @@ require("nvim-treesitter.configs").setup({
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<C-Space>",
+            node_incremental = "<C-Space>",
+        },
+    },
     textobjects = {
         select = {
             enable = true,
