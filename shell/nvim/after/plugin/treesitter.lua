fe7ff62346a0732d27f21d0fc22a637598284b03
Arkadiusz Chmura <arekchmura@gmail.com>
2023-07-13 21:09:50 +0200

    Fix indenting

--- shell/nvim/after/plugin/treesitter.lua
+++ shell/nvim/after/plugin/treesitter.lua
@@ -5,6 +5,9 @@ require("nvim-treesitter.configs").setup({
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+    indent = {
+        enable = true,
+    },
     context_commentstring = {
         enable = true,
     },
