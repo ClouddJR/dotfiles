713f19e6d17e77cb318c4738503d93aa3d1d7491
Arkadiusz Chmura <arekchmura@gmail.com>
2023-10-15 18:23:10 +0200

    Update the telescope plugin

--- shell/nvim/lua/cloudjr/packer.lua
+++ shell/nvim/lua/cloudjr/packer.lua
@@ -44,7 +44,7 @@ return require("packer").startup(function(use)
 
     use {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.1",
+        tag = "0.1.4",
         requires = {
             { "nvim-telescope/telescope-live-grep-args.nvim" },
             {
