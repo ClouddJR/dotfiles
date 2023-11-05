44e73ce41a5169666eb8078e8c38da94431ff37f
Arkadiusz Chmura <arekchmura@gmail.com>
2023-11-05 15:01:18 +0100

    Highlight on yanking

--- shell/nvim/lua/cloudjr/set.lua
+++ shell/nvim/lua/cloudjr/set.lua
@@ -29,3 +29,13 @@ vim.opt.signcolumn = "auto"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = "*",
+})
