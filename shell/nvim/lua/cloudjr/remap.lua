42842f547ba7fe7484ac28cef534d5ede9a8c2e0
Arkadiusz Chmura <arekchmura@gmail.com>
2023-11-05 15:00:49 +0100

    Easier navigation with wrapped words

--- shell/nvim/lua/cloudjr/remap.lua
+++ shell/nvim/lua/cloudjr/remap.lua
@@ -20,3 +20,6 @@ vim.keymap.set("i", "<C-d>", "<C-k>")
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
