846204eccc0475df5b368226bd05e1f1b9eb0dc0
Arkadiusz Chmura <arekchmura@gmail.com>
2024-03-12 18:59:47 +0100

    Add live grep args to telescope

--- shell/nvim/lua/plugins/telescope.lua
+++ shell/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-ui-select.nvim",
+        "nvim-telescope/telescope-live-grep-args.nvim",
         "nvim-tree/nvim-web-devicons",
         {
             "nvim-telescope/telescope-fzf-native.nvim",
@@ -12,7 +13,11 @@ return {
         },
     },
     config = function()
-        require("telescope").setup({
+        local telescope = require("telescope")
+        local builtin = require("telescope.builtin")
+        local lga_shortcuts = require("telescope-live-grep-args.shortcuts")
+
+        telescope.setup({
             defaults = {
                 path_display = { "truncate" },
             },
@@ -20,18 +25,27 @@ return {
                 ["ui-select"] = {
                     require("telescope.themes").get_dropdown(),
                 },
+                live_grep_args = {
+                    auto_quoting = true,
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = require("telescope-live-grep-args.actions").quote_prompt(),
+                        },
+                    },
+                },
             },
         })
 
-        pcall(require("telescope").load_extension, "fzf")
-        pcall(require("telescope").load_extension, "ui-select")
+        pcall(telescope.load_extension, "fzf")
+        pcall(telescope.load_extension, "ui-select")
+        pcall(telescope.load_extension, "live_grep_args")
 
-        local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
         vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
         vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sw", lga_shortcuts.grep_word_under_cursor, { desc = "[S]earch current [W]ord" })
+        vim.keymap.set("v", "<leader>sv", lga_shortcuts.grep_visual_selection, { desc = "[S]earch current [V]isual selection" })
+        vim.keymap.set("n", "<leader>sg", telescope.extensions.live_grep_args.live_grep_args, { desc = "[S]earch by [G]rep" })
         vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
         vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
         vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
@@ -44,13 +58,6 @@ return {
         vim.keymap.set("n", "<leader>s.", function()
             builtin.oldfiles({ only_cwd = true })
         end, { desc = '[S]earch Recent Files ("." for repeat)' })
-
-        vim.keymap.set("n", "<leader>s/", function()
-            builtin.live_grep({
-                grep_open_files = true,
-                prompt_title = "Live Grep in Open Files",
-            })
-        end, { desc = "[S]earch [/] in Open Files" })
 
         vim.keymap.set("n", "<leader>sn", function()
             builtin.find_files({ cwd = vim.fn.stdpath("config") })
